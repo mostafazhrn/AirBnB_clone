dcbd0812fd7a2c3a34767ee6c32cb37b0d830d82
ELHILALICODE <133546427+ELHILALICODE@users.noreply.github.com>
2024-01-13 16:06:09 +0100

    Add files via upload

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import uuid
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strftime(v, tform))
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
+
+    def __str__(self):
+        clname = self__class__.__name__
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        modls.storage.save()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
