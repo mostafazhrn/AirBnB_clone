ca3bb0a0d98d7e2dd18e1e171dc691d55977c902
ELHILALICODE <133546427+ELHILALICODE@users.noreply.github.com>
2024-01-14 10:22:22 +0100

    console.py

--- console.py
+++ console.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """THis module shall contain the console class"""
 import cmd
-import models
 import re
-import json
-import os
-from os import getenv
+from shlex import split
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -13,8 +11,24 @@ from models.state import State
 from models.city import City
 from models.review import Review
 from models.amenity import Amenity
-from models import storage
-from shlex import split
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
 
 
 class HBNBCommand(cmd.Cmd):
@@ -105,3 +119,7 @@ class HBNBCommand(cmd.Cmd):
                 storage.all()[key].save()
             else:
                 print("** no instance found **")
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
