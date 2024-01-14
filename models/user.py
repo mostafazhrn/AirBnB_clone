611ce822abd1581318cfc5814a218eb7991da58f
mostafazhrn <mostafazhrn@gmail.com>
2024-01-14 10:51:31 -0500

    updateUser

--- models/user.py
+++ models/user.py
@@ -2,10 +2,9 @@
 """
 This module shall contain the User class
 """
+import models
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from hashlib import md5
+from os import getenv
 
 
 class User(BaseModel):
@@ -16,18 +15,7 @@ class User(BaseModel):
         first_name (str): This represent the first name
         last_name (str): This represent the last name
     """
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    def __init__(self, *args, **kwargs):
-        """This shall start the User class"""
-        super().__init__(*args, **kwargs)
-        self.password = md5(self.password.encode()).hexdigest()
-
-    def __setattr__(self, name, value):
-        """This shall set the password"""
-        if name == 'password':
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(name, value)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
