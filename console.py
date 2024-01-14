8287ca180c87c78a4d49845eab1258e7dbc990b3
mostafazhrn <mostafazhrn@gmail.com>
2024-01-14 07:26:58 -0500

    updateConsole

--- console.py
+++ console.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""THis module shall contain the console class"""
+"""This module shall contain the console class"""
 import cmd
-import re
-from shlex import split
-from models import storage
+import models
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -11,115 +9,109 @@ from models.state import State
 from models.city import City
 from models.review import Review
 from models.amenity import Amenity
+from datetime import datetime
+import shlex
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
-
+clsses = {"BaseModel": BaseModel, "User": User, "Place": Place,
+            "State": State, "City": City, "Review": Review, "Amenity": Amenity}
 
 
 class HBNBCommand(cmd.Cmd):
-    """This is the console class
+    """This is the HBNBCommand class
     Attributes:
         prompt (str): This represent the prompt
     """
-    prompt = '(hbnb) '
-    __classes = {
-        "BaseModel",
-        "User",
-        "Place",
-        "State",
-        "City",
-        "Amenity",
-        "Review"
-    }
+    prompt = "(hbnb)"
 
     def do_quit(self, arg):
-        """This will exit the program"""
+        """This shall exit the program"""
         return True
-
+    
     def do_EOF(self, arg):
-        """This will exit the program"""
+        """This shall exit the program"""
         return True
-
+    
     def emptyline(self):
-        """This will do nothing"""
+        """This shall do nothing"""
         pass
 
     def do_create(self, arg):
-        """This shall create a new instance of BaseModel
-        and print the id
-        """
-        argp = arg.split()
-        if len(argp) == 0:
+        """This shall create a new instance of BaseModel,
+        save it (to the JSON file) and prints the id"""
+        arg_pls = arg.split()
+        if len(arg_pls) == 0:
             print("** class name missing **")
-        elif argp[0] not in self.__classes:
+        elif arg_pls[0] not in clsses:
             print("** class doesn't exist **")
         else:
-            new = eval(argp[0])()
+            new = clsses[arg_pls[0]]()
             new.save()
             print(new.id)
 
     def do_show(self, arg):
-        """This will print the string representation of an instance"""
-        argp = arg.split()
-        if len(argp) == 0:
+        """This shall print the string representation of an instance
+        based on the class name and id"""
+        arg_pls = arg.split()
+        if len(arg_pls) == 0:
             print("** class name missing **")
-        elif argp[0] not in self.__classes:
+        elif arg_pls[0] not in clsses:
             print("** class doesn't exist **")
-        elif len(argp) == 1:
+        elif len(arg_pls) == 1:
             print("** instance id missing **")
         else:
-            key = "{}.{}".format(argp[0], argp[1])
-            if key in storage.all():
-                print(storage.all()[key])
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """This shall delete an instance based on the class name and id"""
+        arg_pls = arg.split()
+        if len(arg_pls) == 0:
+            print("** class name missing **")
+        elif arg_pls[0] not in clsses:
+            print("** class doesn't exist **")
+        elif len(arg_pls) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(arg_pls[0], arg_pls[1])
+            if key in models.storage.all():
+                del models.storage.all()[key]
+                models.storage.save()
             else:
                 print("** no instance found **")
 
     def do_all(self, arg):
-        """This will print all string representation of all instances"""
-        argp = arg.split()
-        if len(argp) == 0:
-            print([str(v) for v in storage.all().values()])
-        elif argp[0] not in self.__classes:
+        """This shall print all string representation of all instances
+        based or not on the class name"""
+        arg_pls = arg.split()
+        if len(arg_pls) == 0:
+            print([str(v) for v in models.storage.all().values()])
+        elif arg_pls[0] not in clsses:
             print("** class doesn't exist **")
         else:
-            print([str(v) for k, v in storage.all().items() if argp[0] in k])
-
+            print([str(v) for k, v in models.storage.all().items()
+                   if k.split('.')[0] == arg_pls[0]])
+            
     def do_update(self, arg):
-        """This will update an instance based on the class name and id"""
-        argp = split(arg)
-        if len(argp) == 0:
+        """This shall update an instance based on the class name and id
+        by adding or updating attribute"""
+        arg_pls = shlex.split(arg)
+        if len(arg_pls) == 0:
             print("** class name missing **")
-        elif argp[0] not in self.__classes:
+        elif arg_pls[0] not in clsses:
             print("** class doesn't exist **")
-        elif len(argp) == 1:
+        elif len(arg_pls) == 1:
             print("** instance id missing **")
-        elif len(argp) == 2:
+        elif len(arg_pls) == 2:
             print("** attribute name missing **")
-        elif len(argp) == 3:
+        elif len(arg_pls) == 3:
             print("** value missing **")
         else:
-            key = "{}.{}".format(argp[0], argp[1])
-            if key in storage.all():
-                setattr(storage.all()[key], argp[2], argp[3])
-                storage.all()[key].save()
+            key = "{}.{}".format(arg_pls[0], arg_pls[1])
+            if key in models.storage.all():
+                obj = models.storage.all()[key]
+                setattr(obj, arg_pls[2], arg_pls[3])
+                obj.save()
             else:
                 print("** no instance found **")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
