db4dcdf54806754b16dd6756a6e246c7124dedd0
mostafazhrn <mostafazhrn@gmail.com>
2024-01-14 10:40:24 -0500

    pystyleUser.py

--- models/user.py
+++ models/user.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""
+This module shall contain the User class
+"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel):
-    """Represent a User.
-
+    """This is the User class
     Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+        email (str): This represent the email
+        password (str): This represent the password
+        first_name (str): This represent the first name
+        last_name (str): This represent the last name
     """
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self, *args, **kwargs):
+        """This shall start the User class"""
+        super().__init__(*args, **kwargs)
+        self.password = md5(self.password.encode()).hexdigest()
+
+    def __setattr__(self, name, value):
+        """This shall set the password"""
+        if name == 'password':
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
